ef9a1e2102f84de8d6ea601290e4815cd03eb1d5
Arin Dewi Jayanti <arindewijayanti1997@gmail.com>
2021-03-23 12:56:45 +0700

    background login

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -32,7 +32,7 @@
 <body>
 	
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('login/images/bg-01.jpg');">
+		<div class="container-login100" style="background-image: url('theme/login/images/bg-01.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					LOGIN
