5f152198e0ab53285a079d0ed47a99386a4bd38d
Arin Dewi Jayanti <arindewijayanti1997@gmail.com>
2021-05-03 22:03:48 +0700

    ajhh

--- application/views/gantipassword/gantipassword.php
+++ application/views/gantipassword/gantipassword.php
@@ -78,36 +78,3 @@ $this->load->view('include/header');
 
 <?php $this->load->view('include/footer'); ?>
 
-<!-- Vendor JS Files -->
-<script src="<?php echo base_url('theme/mamba/assets/vendor/jquery/jquery.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/jquery.easing/jquery.easing.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/php-email-form/validate.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/jquery-sticky/jquery.sticky.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/venobox/venobox.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/waypoints/jquery.waypoints.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/counterup/counterup.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/owl.carousel/owl.carousel.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/isotope-layout/isotope.pkgd.min.js')?>"></script>
-  <script src="<?php echo base_url('theme/mamba/assets/vendor/aos/aos.js')?>"></script>
-
-  <!-- Template Main JS File -->
-  <script src="<?php echo base_url('theme/mamba/assets/js/main.js')?>"></script>
-
-<!-- Bootstrap core JavaScript-->
-
-<script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
-
-<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-<!-- Core plugin JavaScript-->
-<script src="<?php echo base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
-
-<!-- Page level plugin JavaScript-->
-<script src="<?php echo base_url('assets/vendor/chart.js/Chart.min.js'); ?>"></script>
-<script src="<?php echo base_url('assets/vendor/datatables/jquery.dataTables.js'); ?>"></script>
-<script src="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.js'); ?>"></script>
-<!-- Custom scripts for all pages-->
-<script src="<?php echo base_url('assets/js/sb-admin.min.js'); ?>"></script>
-<!-- Custom scripts for this page-->
-<script src="<?php echo base_url('assets/js/sb-admin-datatables.min.js'); ?>"></script>
-<script src="<?php echo base_url('assets/js/sb-admin-charts.min.js'); ?>"></script>
