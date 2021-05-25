97e446086d8632c41a732ecf27aaa58ca5d84d0e
yulia <yuliaariska1998@gmail.com>
2021-05-25 15:47:03 +0700

    a

--- application/views/detailpengadaan/add.php
+++ application/views/detailpengadaan/add.php
@@ -22,12 +22,9 @@ $this->load->view('pptk/barangpersediaan/menu');
         <div class="card-body">
           <div class="table-responsive">
              <div class="container">
-        <form action="<?php echo base_url()?>detailpengadaan/simpan_detailpengadaan/<?php echo $hasilparsing; ?>" method="post" enctype="multipart/form-data">
-              <div class="form-group">
-              <div class="form-row">
-                  <div class="col-md-6">
+             <div class="col-md-6">
                     <label for="id_ssh">Nama Barang</label>
-                    <select class="form-control form-control-sm" id="id_ssh" name="id_ssh" required />
+                    <select class="theSelect" id="id_ssh" name="id_ssh" required />
                     
                     <option>Silahkan Pilih Barang</option>
                         <?php $id_ssh = $this->db->query("SELECT * FROM tbl_ssh ORDER BY Namabarang_ssh");
@@ -37,15 +34,12 @@ $this->load->view('pptk/barangpersediaan/menu');
                          <?php endforeach; ?>
                        </select>
                         </div>
-                  <div class="col-md-6">
-                    <label for="total_barang_in">Total Barang</label>
-                    <input class="form-control" id="total_barang_in" type="text" aria-describedby="nameHelp" name="total_barang_in" required/>
+                        <br>
+                  <div class="col-md-5">
+                    <label for="total_barang">Total Barang</label>
+                    <input class="form-control" id="total_barang" type="text" aria-describedby="nameHelp" name="total_barang" required/>
                   </div>
-                </div>
-              </div>
-
-              <div class="form-group">
-            <div class="form-row">
+              <br>
               <div class="col-md-2">
                 <input class="form-control btn btn-primary" type="submit" value="Simpan" name="btnSimpan" >
               </div>
@@ -62,6 +56,8 @@ $this->load->view('pptk/barangpersediaan/menu');
 
 </div>
     </section>
-
+    <script>
+		$(".theSelect").select2();
+	</script>
 <?php $this->load->view('include/footer'); ?>
 
--- application/views/include/header.php
+++ application/views/include/header.php
@@ -26,6 +26,10 @@
   <link href="<?php echo base_url().'assets/css/jquery.datatables.min.css'?>" rel="stylesheet" type="text/css"/>
   <link href="<?php echo base_url().'assets/css/dataTables.bootstrap.css'?>" rel="stylesheet" type="text/css"/>
  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/css/select2.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js"></script>
+
 <!-- ======= Top Bar ======= -->
 <section id="topbar" class="d-none d-lg-block">
     <div class="container clearfix">
